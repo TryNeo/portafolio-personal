73b8a2886e37ec02c936ea5a084f2a89dec90945
Josue Lopez <tshadez@hotmail.com>
2021-08-15 12:24:13 -0500

    Terinado el modulo testimoonios

---
+++ static/dashboard/assets/ajax/functions_testimonial.js
@@ -0,0 +1,24 @@
+$(function(){
+    const fieldsToValidate = ['commentary']
+
+    let validatorServerSide = $('form.needs-validation').jbvalidator({
+        errorMessage: true,
+        successClass: true,
+    });
+    validatorServerSide.validator.custom = function(el, event){
+
+        if($(el).is('[name=commentary]')){
+            let value= $(el).val()
+
+            if (!validateStringLength(value,10)){
+                return 'El comentario '+value+' debe ser mas largo';
+            }
+            
+        }
+       
+    }
+    sendingDataServerSide('#fntTestimonialCreate',validatorServerSide,fieldsToValidate,true,table_testimonial);
+    sendingDataServerSide('#fntTestimonialUpdate',validatorServerSide,fieldsToValidate,true,table_testimonial);
+    deleteDataofServerSide('#fntTestimonialDelete',true,table_testimonial);
+
+});
